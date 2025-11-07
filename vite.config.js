88cf58a67a4b09bb3f5cf0f8d262d40242c23869
Kathan Purohit <163106847+KathanPurohit@users.noreply.github.com>
2025-11-07 10:50:54 +0530

    Change base path for Render deployment
    Updated base path for deployment to Render.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Fsd-miniProject/",  // name of your repo
+  base: "/",   // ✅ Required for Render deployment
 });
